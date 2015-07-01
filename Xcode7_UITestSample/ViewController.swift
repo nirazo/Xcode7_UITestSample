a57cc1fe622ebe0df258628d4645f5702d8c0adb
Kenzo Nirasawa <kemuzo.nira@gmail.com>
2015-07-01 23:54:16 +0900

    Initial Commit

---
+++ Xcode7_UITestSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode7_UITestSample
+//
+//  Created by Kenzo on 2015/07/01.
+//  Copyright © 2015年 Karuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
